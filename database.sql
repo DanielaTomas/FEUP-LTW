b1a95e13f082b4e6f76c9acf6a5687fe3d496470
Daniela Tomás <92220581+DanielaTomas@users.noreply.github.com>
2022-05-03 19:21:03 +0100

    update database

--- database.sql
+++ database.sql
@@ -19,23 +19,22 @@ drop table if exists Restaurant;
 
 create table Restaurant(
     RestaurantId int PRIMARY KEY,
-    RestaurantName varchar(80) constraint restaurantname_nn NOT NULL,
+    RestaurantName varchar(80) NOT NULL,
     RestaurantAddress varchar(70),
     Category varchar(30),
-    UserId int constraint userid_nn NOT NULL references Administrator ON DELETE CASCADE
-                                                                      ON UPDATE CASCADE   
+    UserId int NOT NULL references Administrator ON DELETE CASCADE
+                                                 ON UPDATE CASCADE  
 );
 
 create table User(
     UserId int PRIMARY KEY,
-    FirstLastName varchar(60) constraint firstlastname_nn NOT NULL,
-    Username varchar(50) constraint username_nn NOT NULL
-                         constraint username_u UNIQUE,
-    Password varchar(40) NOT NULL constraint password_nn NOT NULL,
+    FirstLastName varchar(60) NOT NULL,
+    Username varchar(50) NOT NULL UNIQUE,
+    Password varchar(40) NOT NULL,
     UserAddress varchar(70),
     PhoneNumber varchar(24),
-    RestaurantId int constraint restaurantid_nn NOT NULL references Restaurant ON DELETE CASCADE
-                                                                               ON UPDATE CASCADE 
+    RestaurantId int NOT NULL references Restaurant ON DELETE CASCADE
+                                                    ON UPDATE CASCADE 
 );
 
 create table Administrator(
@@ -45,80 +44,80 @@ create table Administrator(
 
 create table Orders(
     OrderId int PRIMARY KEY,
-    Status varchar(60) constraint status_nn NOT NULL,
-    RestaurantId int constraint restaurantid_nn NOT NULL references Restaurant ON DELETE CASCADE
-                                                                               ON UPDATE CASCADE,  
-    UserId int constraint userid_nn NOT NULL references User ON DELETE CASCADE
-                                                             ON UPDATE CASCADE                                                                                
+    OrderStatus varchar(60) NOT NULL,
+    RestaurantId int NOT NULL references Restaurant ON DELETE CASCADE
+                                                    ON UPDATE CASCADE,  
+    UserId int NOT NULL references User ON DELETE CASCADE
+                                        ON UPDATE CASCADE                                                                                
 );
 
 create table Review(
     ReviewId int PRIMARY KEY,
     Score float constraint score_c CHECK(Score >= 0 and Score <= 5),
     ReviewText varchar(250),
-    RestaurantId int constraint restaurantid_nn NOT NULL references Restaurant ON DELETE CASCADE
-                                                                               ON UPDATE CASCADE          
+    RestaurantId int NOT NULL references Restaurant ON DELETE CASCADE
+                                                    ON UPDATE CASCADE          
 );
 
 create table Comment(
     CommentId int PRIMARY KEY,
     AdminComment varchar(250),
-    UserId int constraint userid_nn NOT NULL references Administrator ON DELETE CASCADE                                                                                
-                                                                      ON UPDATE CASCADE
+    UserId int NOT NULL references Administrator ON DELETE CASCADE                                                                                
+                                                 ON UPDATE CASCADE
 );
 
 create table Menu(
     MenuId int PRIMARY KEY,
-    MenuType text constraint menutype_c CHECK((MenuType = "APPETIZER" OR
-                                               MenuType = "SOUP" OR
-                                               MenuType = "MEAT" OR
-                                               MenuType = "FISH" OR
-                                               MenuType = "DRINK" OR
-                                               MenuType = "DESSERT"))
+    MenuType text CHECK((MenuType = "APPETIZER" OR
+                         MenuType = "SOUP" OR
+                         MenuType = "MEAT" OR
+                         MenuType = "FISH" OR
+                         MenuType = "DRINK" OR
+                         MenuType = "DESSERT"))
 );
 
 create table Promotion(
     PromotionId int PRIMARY KEY,
-    Value float constraint value_nn NOT NULL
-                constraint value_c CHECK(Value >= 0)
+    Discount float NOT NULL
+                   CHECK(Discount >= 0)
 );
 
 create table Dish(
     DishId int PRIMARY KEY,
-    DishName varchar(80) constraint dishname_nn NOT NULL,
-    Price float constraint price_nn NOT NULL
-                constraint price_c CHECK(price >= 0),          
-    DishDescription varchar(150) constraint dishdescription_nn NOT NULL,
+    DishName varchar(80) NOT NULL,
+    Price float NOT NULL
+                CHECK(price >= 0),          
+    DishDescription varchar(150) NOT NULL,
     Photo varchar(2083),
-    PromotionId int constraint promotionid_nn NOT NULL references Promotion ON DELETE CASCADE
-                                                                               ON UPDATE CASCADE,  
-    MenuId int constraint menuid_nn NOT NULL references Menu ON DELETE CASCADE
-                                                             ON UPDATE CASCADE
+    PromotionId int NOT NULL references Promotion ON DELETE CASCADE
+                                                  ON UPDATE CASCADE,  
+    MenuId int NOT NULL references Menu ON DELETE CASCADE
+                                        ON UPDATE CASCADE
 );
 
 -- ** --
 
 create table RestaurantMenu(
-    RestaurantId int constraint restaurantid_nn NOT NULL references Restaurant ON DELETE CASCADE
-                                                                               ON UPDATE CASCADE,  
-    MenuId int constraint menuid_nn NOT NULL references Menu ON DELETE CASCADE
-                                                             ON UPDATE CASCADE,  
+    RestaurantId int NOT NULL references Restaurant ON DELETE CASCADE
+                                                    ON UPDATE CASCADE,  
+    MenuId int NOT NULL references Menu ON DELETE CASCADE
+                                        ON UPDATE CASCADE,  
     PRIMARY KEY (RestaurantId, MenuId)
 );
 
 create table DishOrder(
-    DishId int constraint dishid_nn NOT NULL references Dish ON DELETE CASCADE
-                                                             ON UPDATE CASCADE,  
-    OrderId int constraint orderid_nn NOT NULL references Orders ON DELETE CASCADE
-                                                                 ON UPDATE CASCADE,  
+    DishId int NOT NULL references Dish ON DELETE CASCADE
+                                        ON UPDATE CASCADE,  
+    OrderId int NOT NULL references Orders ON DELETE CASCADE
+                                           ON UPDATE CASCADE,  
     PRIMARY KEY (DishId, OrderId)
 );
 
 create table PromotionAdmin(
-    PromotionId int constraint promotionid_nn NOT NULL references Promotion ON DELETE CASCADE
-                                                                            ON UPDATE CASCADE,  
-    UserId int constraint userid_nn NOT NULL references Administrator ON DELETE CASCADE
-                                                                      ON UPDATE CASCADE,  
+    PromotionId int NOT NULL references Promotion ON DELETE CASCADE
+                                                  ON UPDATE CASCADE,  
+    UserId int NOT NULL references Administrator ON DELETE CASCADE
+                                                 ON UPDATE CASCADE,  
     PRIMARY KEY (PromotionId, UserId)
 );
 
